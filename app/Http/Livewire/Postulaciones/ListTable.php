b3d4caa28b20741fdfe46c1272416ee9bd8ce8d3
brunomont <brunoadrian.mont@gmail.com>
2023-07-14 22:54:38 -0500

    feat: complete postulaciones listtable component

--- app/Http/Livewire/Postulaciones/ListTable.php
+++ app/Http/Livewire/Postulaciones/ListTable.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Postulaciones;
 
 use App\Http\Traits\WithSorting;
+use App\Models\Postulacion;
 use App\Models\Postulante;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -12,6 +13,11 @@ class ListTable extends Component
     use WithPagination;
     use WithSorting;
     public $search = '';
+    public $confirmingPostulacionesDeletion = false;
+    public $confirmingPostulacionDeletion = false;
+    public $selectedPostulante = null;
+    public $selectedPostulacion = null;
+    public $expandedRows = [];
     protected $listeners = ['sort', 'search'];
     protected $queryString = [
 
@@ -33,14 +39,47 @@ class ListTable extends Component
         $this->resetPage();
     }
 
+    public function toggleExpanded($id)
+    {
+        if (isset($this->expandedRows[$id])) {
+            unset($this->expandedRows[$id]);
+        } else {
+            $this->expandedRows[$id] = true;
+        }
+    }
+
+    public function confirmPostulacionesDeletion(Postulante $postulante)
+    {
+        $this->confirmingPostulacionesDeletion = true;
+        $this->selectedPostulante = $postulante;
+
+        $this->emit('open-custom-modal', 'confirm-postulaciones-deletion');
+    }
+
+    public function confirmPostulacionDeletion(Postulacion $postulacion)
+    {
+        $this->selectedPostulacion = $postulacion;
+        $this->confirmingPostulacionDeletion = true;
+    }
+    public function cerrarModal()
+    {
+        $this->confirmingPostulacionesDeletion = false;
+        $this->selectedPostulante = null;
+        $this->confirmingPostulacionDeletion = false;
+        $this->selectedPostulacion = null;
+    }
 
     public function render()
     {
+        $postulantes = Postulante
+            ::has('postulaciones')
+            ->where('nombre', 'LIKE', "%{$this->search}%")->orderBy($this->sortBy, $this->sortDirection)
+            ->with(['postulaciones' => function ($query) {
+                $query->orderBy('fecha_postulacion', 'desc');
+            }])
+            ->paginate(10);
         return view('livewire.postulaciones.list-table', [
-            'postulantes' => Postulante
-                ::has('postulaciones')
-                ->where('nombre', 'LIKE', "%{$this->search}%")->orderBy($this->sortBy, $this->sortDirection)
-                ->paginate(10),
+            'postulantes' => $postulantes,
         ]);
     }
 }
